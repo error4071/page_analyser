ebcd538523b3b5ec1f4a39115b915abee137ccf8
error4071 <sergey_tsoy87@mail.ru>
2023-11-09 20:25:42 +0500

    app add

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,21 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World!"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
