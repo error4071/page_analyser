1feba6a2d4f02b75141dc1426517e9dd0150d118
error4071 <sergey_tsoy87@mail.ru>
2023-11-11 15:43:02 +0500

    App fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,11 @@ import io.javalin.Javalin;
 
 public final class App {
 
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "3000");
+        return Integer.valueOf(port);
+    }
+
     public static Javalin getApp() {
 
         var app = Javalin.create(config -> {
@@ -16,6 +21,6 @@ public final class App {
 
     public static void main(String[] args) {
         Javalin app = getApp();
-        app.start(7070);
+        app.start(getPort());
     }
 }
