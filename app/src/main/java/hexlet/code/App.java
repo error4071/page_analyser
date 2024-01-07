3d19c56b118b3b47a50c8c4575ef7fa332771e7d
error4071 <sergey_tsoy87@mail.ru>
2024-01-08 00:18:45 +0500

    try #11

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.extern.slf4j.Slf4j;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.resolve.ResourceCodeResolver;
@@ -20,11 +19,20 @@ import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.stream.Collectors;
 
-@Slf4j
 public final class App {
+
     private static String getDatabaseUrl() {
-        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:hexlet_project;DB_CLOSE_DELAY=-1;");
     }
+
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals("production");
+    }
+
     private static String readResourceFile(String fileName) throws IOException {
         var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
@@ -40,18 +48,27 @@ public final class App {
 
     public static Javalin getApp() throws IOException, SQLException {
 
+        JavalinJte.init(createTemplateEngine());
+
         var hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(getDatabaseUrl());
-
+        if (isProduction()) {
+            String username = System.getenv("JDBC_DATABASE_USERNAME");
+            hikariConfig.setUsername(username);
+            String password = System.getenv("JDBC_DATABASE_PASSWORD");
+            hikariConfig.setPassword(password);
+        }
         var dataSource = new HikariDataSource(hikariConfig);
-        String sql = readResourceFile("schema.sql");
 
-        log.info(sql);
+        var schemaSql = readResourceFile("schema.sql");
+
         try (var connection = dataSource.getConnection();
              var statement = connection.createStatement()) {
-            statement.execute(sql);
+            statement.execute(schemaSql);
         }
+
         BaseRepository.dataSource = dataSource;
+
 
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
@@ -60,8 +77,6 @@ public final class App {
         app.before(ctx -> {
             ctx.contentType("text/html; charset=utf-8");
         });
-
-        JavalinJte.init(createTemplateEngine());
 
         JavalinJte.init(createTemplateEngine());
 
@@ -77,9 +92,9 @@ public final class App {
     private static TemplateEngine createTemplateEngine() {
         ClassLoader classLoader = App.class.getClassLoader();
         ResourceCodeResolver codeResolver = new ResourceCodeResolver("jte", classLoader);
-        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
-        return templateEngine;
+        return TemplateEngine.create(codeResolver, ContentType.Html);
     }
+
 
     public static void main(String[] args) throws SQLException, IOException {
         Javalin app = getApp();
