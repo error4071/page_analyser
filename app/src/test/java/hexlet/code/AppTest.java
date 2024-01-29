73783fa1ef2ac360311f84241e2c44b2b2994343
error4071 <sergey_tsoy87@mail.ru>
2024-01-30 00:39:40 +0500

    test fix-2

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -16,8 +16,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -146,7 +144,7 @@ public final class AppTest {
         JavalinTest.test(app, (server, client) -> {
             var response = client.post("/urls/1/checks");
 
-            assertThat(response.body().string()).doesNotContain("Example Domain");
+            assertThat(response.body().string()).doesNotContain("Анализатор страниц");
         });
 
         assertThat(UrlRepositoryCheck.getEntities(1L));
