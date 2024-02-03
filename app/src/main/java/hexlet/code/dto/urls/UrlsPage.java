da32149d86b2b66fedab436e6c7e841162ab8f05
error4071 <sergey_tsoy87@mail.ru>
2024-02-03 22:56:00 +0500

    test update again

--- app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
@@ -13,13 +13,8 @@ import java.util.Map;
 @Getter
 
 public class UrlsPage extends BasePage {
-    private Long pageNumber;
-    private List<Url> pagedUrls;
     private List<Url> urls;
     private Map<Long, UrlCheck> latestChecks;
-    private List<UrlCheck> lastCheck;
-    private String conditionNext;
-    private String conditionBack;
 
     public UrlsPage(List<Url> pagedUrls, Long pageNumber, List<UrlCheck> lastCheck,
                     String conditionNext, String conditionBack) {
