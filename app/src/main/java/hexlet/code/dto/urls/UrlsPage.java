d4204e71df30d40e66dbe920a32b8498c1c70aae
error4071 <sergey_tsoy87@mail.ru>
2024-02-03 22:39:04 +0500

    test update again

--- app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
@@ -3,7 +3,6 @@ package hexlet.code.dto.urls;
 import hexlet.code.dto.BasePage;
 import hexlet.code.model.Url;
 import hexlet.code.model.UrlCheck;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
