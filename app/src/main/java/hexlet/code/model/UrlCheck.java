cb1abccde5d390c820d653959ddbf411d5b5d9ae
error4071 <sergey_tsoy87@mail.ru>
2024-02-07 01:25:44 +0500

    testing test

--- app/src/main/java/hexlet/code/model/UrlCheck.java
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -2,22 +2,30 @@ package hexlet.code.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.AllArgsConstructor;
 
 import java.sql.Timestamp;
 
-@AllArgsConstructor
 @Getter
 @Setter
 
 public class UrlCheck {
 
+    private Long id;
     private int statusCode;
     private String title;
     private String h1;
     private String description;
-    private Long id;
+    private Long urlId;
     private Timestamp createdAt;
+
+    public UrlCheck(int statusCode, String title, String h1, String description, Long urlId, Timestamp createdAt) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+        this.urlId = urlId;
+        this.createdAt = createdAt;
+    }
 
     public UrlCheck() {
     }
