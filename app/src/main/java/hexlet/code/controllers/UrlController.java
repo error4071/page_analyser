715d2e884e5850e4c8fad357e6c1d0bd68d6f1f4
error4071 <sergey_tsoy87@mail.ru>
2024-02-15 21:33:01 +0500

    Project mini  changes

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -20,7 +20,6 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UrlController {
